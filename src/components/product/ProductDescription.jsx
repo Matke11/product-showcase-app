3ddfb9c96d20d87786e98457cc212b325ddd2719
Milan Matic <mmatic@aliceandsmithmetalab.com>
2024-12-07 16:18:06 +0100

    description section styling

--- src/components/product/ProductDescription.jsx
+++ src/components/product/ProductDescription.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const ProductDescription = ({ descriptionShort, descriptionLong }) => {
   return (
     <Box component="section" sx={{ p: 3 }}>
-      <p>Description</p>
-      <p>{descriptionShort}</p>
-      <p>{descriptionLong}</p>
+      <Typography
+        variant="h6"
+        sx={{
+          textTransform: "uppercase",
+          color: "primary.main",
+          fontSize: "0.75rem",
+          fontWeight: "bold",
+        }}
+      >
+        Description
+      </Typography>
+
+      <Typography variant="body2" sx={{ marginBottom: 1 }}>
+        {descriptionShort}
+      </Typography>
+      <Typography variant="body2" sx={{ marginTop: 1 }}>
+        {descriptionLong}
+      </Typography>
     </Box>
   );
 };
